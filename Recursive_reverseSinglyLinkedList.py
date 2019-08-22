4cebfd23b29df0c2ca7114ca4d003478a9606eac
Shubhra Rajadhyaksha <shubhy30@gmail.com>
2019-08-21 23:02:51 -0700

    Reverse a singly linked list iteratively

---
+++ Recursive_reverseSinglyLinkedList.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or head.next==None:
+            return head
+        #Reverse the rest of the list not including node
+        reversing_everything_else=self.reverseList(head.next)
+        #Use this reversed end of the list and add the current node
+        
+        # The node after reversed list should the current node
+        head.next.next=head
+        
+        head.next=None
+        return reversing_everything_else
+        
+    
